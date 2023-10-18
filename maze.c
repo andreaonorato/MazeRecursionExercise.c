4b77ec05864177670424794c366d85618a63a8e4
Andrea Onorato <63243990+andreaonorato@users.noreply.github.com>
2023-10-18 19:28:11 +0200

    Update maze.c

--- maze.c
+++ maze.c
@@ -9,163 +9,157 @@
 #define STOP  '#'
 #define PATH  '$'
 
-const int xOff[4] = { 0, 1, 0, -1};
-const int yOff[4] = {-1, 0, 1,  0};
+const int xOff[4] = {0, 1, 0, -1}; // Offset for moving in x-direction
+const int yOff[4] = {-1, 0, 1, 0}; // Offset for moving in y-direction
 
-/* function prototypes */
-void display(char **maze, int nr);
-int move_recur(char **mazeCurr, int stepCurr, char **mazeBest, int stepBest, 
-               int nr, int nc, int row, int col);
-FILE *util_fopen(char *name, char *mode);
-void *util_malloc(int size);
-char *util_strdup(char *src);
+/* Function prototypes */
+void displayMaze(char **maze, int numRows);
+int findShortestPath(char **mazeCurr, int currentStep, char **mazeBest, int bestStep, int numRows, int numCols, int row, int col);
+FILE *openFile(char *filename, char *mode);
+void *allocateMemory(int size);
+char *copyString(char *source);
 
-/*
- *  main program
- */
-int main(int argc, char *argv[])
-{
-  int r=-1, c=-1, i, j, nr, nc, step;
+int main(int argc, char *argv[]) {
+  int numRows = -1, numCols = -1, i, j, startingRow, startingCol, step;
   char **mazeCurr, **mazeBest;
   char line[MAX];
-  FILE *fp;
+  FILE *file;
 
   if (argc < 2) {
-    printf("Error: missing parameter.\n");
-    printf("Run as: %s <maze_file>\n", argv[0]);
+    printf("Error: Missing parameter.\n");
+    printf("Usage: %s <maze_file>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
-  fp = util_fopen(argv[1], "r");
-  fgets(line, MAX, fp);
-  sscanf(line, "%d %d", &nr, &nc);
-  mazeCurr = (char **)util_malloc(nr * sizeof(char *));
-  mazeBest = (char **)util_malloc(nr * sizeof(char *));
-  for (i=0; i<nr; i++) {
-    fgets(line, MAX, fp);
-    mazeCurr[i] = util_strdup(line);
-    mazeBest[i] = util_strdup(line);
-    for (j=0; j<nc; j++) {
+  // Open the maze file for reading
+  file = openFile(argv[1], "r");
+  fgets(line, MAX, file);
+  sscanf(line, "%d %d", &numRows, &numCols);
+  mazeCurr = (char **)allocateMemory(numRows * sizeof(char *));
+  mazeBest = (char **)allocateMemory(numRows * sizeof(char *));
+
+  // Read and initialize the maze
+  for (i = 0; i < numRows; i++) {
+    fgets(line, MAX, file);
+    mazeCurr[i] = copyString(line);
+    mazeBest[i] = copyString(line);
+    for (j = 0; j < numCols; j++) {
       if (mazeCurr[i][j] == START) {
-        mazeCurr[i][j] = ' ';
-        r = i; 
-        c = j;
+        mazeCurr[i][j] = EMPTY;
+        startingRow = i;
+        startingCol = j;
       }
     }
   }
 
-  if (r<0 || c<0) {
-    printf("Error: no startig position found!\n");
+  if (startingRow < 0 || startingCol < 0) {
+    printf("Error: No starting position found!\n");
     exit(EXIT_FAILURE);
   }
-  step = move_recur(mazeCurr, 0, mazeBest, nr*nc, nr, nc, r, c);
+
+  // Find the shortest path
+  step = findShortestPath(mazeCurr, 0, mazeBest, numRows * numCols, numRows, numCols, startingRow, startingCol);
+
+  // Display the solution if found
   if (step > 0) {
     printf("Solution:\n");
-    mazeBest[r][c] = START;
-    display(mazeBest, nr);
+    mazeBest[startingRow][startingCol] = START;
+    displayMaze(mazeBest, numRows);
   } else {
-    printf("NO solution found!\n");
+    printf("No solution found!\n");
   }
 
-  for (r=0; r<nr; r++) {
-    free(mazeCurr[r]);
-    free(mazeBest[r]);
+  // Clean up allocated memory
+  for (i = 0; i < numRows; i++) {
+    free(mazeCurr[i]);
+    free(mazeBest[i]);
   }
   free(mazeCurr);
   free(mazeBest);
   return EXIT_SUCCESS;
 }
 
-/*
- *  recursively move the player up to the maze exit
- *  find the best solution
- */
-int move_recur(char **mazeCurr, int stepCurr, char **mazeBest, int stepBest, 
-               int nr, int nc, int row, int col)
-{
+// Recursive function to find the shortest path
+int findShortestPath(char **mazeCurr, int currentStep, char **mazeBest, int bestStep, int numRows, int numCols, int row, int col) {
   int k, r, c;
 
-  /* Avoid stopping recursion to find ALL solutions */
-  if (stepCurr >= stepBest) {
-    return stepBest;
+  // Avoid further recursion if a solution is already found
+  if (currentStep >= bestStep) {
+    return bestStep;
   }
+
+  // Check if the current position is the exit
   if (mazeCurr[row][col] == STOP) {
-    /* STOP recurring to print-out only ONE solution */
-    /* Print all matrices to have ALL solutions */
-    if (stepCurr < stepBest) {
-      stepBest = stepCurr;
-      for (r=0; r<nr; r++) {
-        for (c=0; c<nc; c++) {
+    if (currentStep < bestStep) {
+      bestStep = currentStep;
+      // Copy the best solution to mazeBest
+      for (r = 0; r < numRows; r++) {
+        for (c = 0; c < numCols; c++) {
           mazeBest[r][c] = mazeCurr[r][c];
         }
       }
     }
-    return stepBest;
-  }
-  if (mazeCurr[row][col] != EMPTY) {
-    return stepBest;
+    return bestStep;
   }
 
+  // Check if the current position is not empty
+  if (mazeCurr[row][col] != EMPTY) {
+    return bestStep;
+  }
+
+  // Mark the current position as visited
   mazeCurr[row][col] = PATH;
-  for (k=0; k<4; k++) {
+
+  // Explore four possible directions
+  for (k = 0; k < 4; k++) {
     r = row + xOff[k];
     c = col + yOff[k];
-    if (r>=0 && r<nr && c>=0 && c<nc) {
-      stepBest = move_recur(mazeCurr, stepCurr+1, mazeBest, stepBest, nr, nc, r, c);
+    if (r >= 0 && r < numRows && c >= 0 && c < numCols) {
+      bestStep = findShortestPath(mazeCurr, currentStep + 1, mazeBest, bestStep, numRows, numCols, r, c);
     }
   }
+
+  // Restore the current position to empty
   mazeCurr[row][col] = EMPTY;
 
-  return stepBest;
+  return bestStep;
 }
 
-/*
- *  write the maze scheme on screen
- */
-void display(char **maze, int nr) 
-{
+// Function to display the maze
+void displayMaze(char **maze, int numRows) {
   int i;
 
-  for (i=0; i<nr; i++) {
+  for (i = 0; i < numRows; i++) {
     printf("%s", maze[i]);
   }
 }
 
-/*
- *  fopen (with check) utility function
- */
-FILE *util_fopen(char *name, char *mode)
-{
-  FILE *fp=fopen(name, mode);
-  if (fp == NULL) {
-    printf("Error opening file %s.\n", name);
+// Utility function to open a file
+FILE *openFile(char *filename, char *mode) {
+  FILE *file = fopen(filename, mode);
+  if (file == NULL) {
+    printf("Error opening file %s.\n", filename);
     exit(EXIT_FAILURE);
-  }  
-  return fp;
+  }
+  return file;
 }
 
-/*
- *  malloc (with check) utility function
- */
-void *util_malloc(int size)
-{
-  void *ptr=malloc(size);
+// Utility function to allocate memory with error handling
+void *allocateMemory(int size) {
+  void *ptr = malloc(size);
   if (ptr == NULL) {
     printf("Memory allocation error.\n");
     exit(EXIT_FAILURE);
-  }  
+  }
   return ptr;
 }
 
-/*
- *  strdup (with check) utility function
- */
-char *util_strdup(char *src)
-{
-  char *dst=strdup(src);
-  if (dst == NULL) {
+// Utility function to duplicate a string with error handling
+char *copyString(char *source) {
+  char *destination = strdup(source);
+  if (destination == NULL) {
     printf("Memory allocation error.\n");
     exit(EXIT_FAILURE);
-  }  
-  return dst;
+  }
+  return destination;
 }
